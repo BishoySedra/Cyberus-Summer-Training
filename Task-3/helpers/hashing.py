309a1318517c1e8bd6d8d064c58fce897ed913f2
BishoySedra <bishosedra0@gmail.com>
2023-08-26 20:50:35 +0300

    Task 3

---
+++ Task-3/helpers/hashing.py
@@ -0,0 +1,9 @@
+import bcrypt
+
+
+def hash_password(password):
+    salt_rounds = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode(),salt_rounds).decode()
+
+def is_password_matched(password, stored_hashed_password):
+    return bcrypt.checkpw(password.encode(), stored_hashed_password.encode())
